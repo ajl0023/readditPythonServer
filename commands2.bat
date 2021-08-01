f81c87c56948fe61a09a23447ce35d999e041857
ajl0023 <austinlee199723@yahoo.com>
2021-08-01 16:32:08 -0700

    addd changes to c2

--- commands2.bat
+++ commands2.bat
@@ -2,4 +2,4 @@ git add .
 git commit -m "few tweaks to get it working on mac os env test"
 git push origin main
 
-REM THIS IS SOME TEST COMMENT
+REM THIS IS SOME TEST COMMENT, this is another change 
